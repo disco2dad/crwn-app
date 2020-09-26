4cd9e0698c8efbf14cb1df55f8536e9fec8bc260
unknown <disco2dad@yahoo.com>
2020-09-25 23:39:39 -0600

    adding a header component with logo

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './header/header.component.jsx';     //May need to move this under components instead of directly in src
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
